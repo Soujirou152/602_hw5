b47614f7c63c1cf1692aebfb84f6269089ef928d
Soujirou152 <her.pengxue@gmail.com>
2016-10-26 14:24:10 -0500

    Finished the rest, #4 and #5

--- 602_hw_4/src/CheckNested.java
+++ 602_hw_4/src/CheckNested.java
@@ -1,13 +1,22 @@
 
-public class CheckNested
-{
-	public static boolean isProperlyNested (String expr)
-	{
-		return false;
+public class CheckNested {
+	public static boolean isProperlyNested(String expr) {
+		if (expr.length() > 0 && expr.charAt(0) == '(' && expr.charAt(expr.length() - 1) == ')') {
+			return true && isProperlyNested(expr.substring(1, expr.length() - 1));
+		} else if (expr.length() == 0 && expr.equals("")) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
-	
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
+		String s = null;
+		StdOut.println("Enter a string of parentheses: i.e. '((()))' which evaluates true, or '()()' which is false ");
 		
+		s = StdIn.readLine();
+		
+		StdOut.println(isProperlyNested(s));
 	}
 }
--- 602_hw_4/src/ConvertNum.java
+++ 602_hw_4/src/ConvertNum.java
@@ -1,14 +1,31 @@
 
-public class ConvertNum
-{
-	public static String convertNum(int number, int base)
-	{
-		return ""; 
+public class ConvertNum {
+	@SuppressWarnings("static-access")
+	public static String convertNum(int number, int base) {
+		String convertString = "0123456789ABCDEF";
+		if (number < base) {
+			char ch;
+			String s;
+			ch = convertString.charAt(number);
+			s = Character.toString(ch);
+			return s;
+		} else {
+			char ch;
+			String s;
+			ch = convertString.charAt(number% base);
+			s = Character.toString(ch);
+			return convertNum(number / base, base) + s;
+		}
 	}
 
-	public static void main(String[] args)
-	{
-
+	public static void main(String[] args) {
+		int num = 0;
+		StdOut.println("Enter an int greater than 0");
+		num = StdIn.readInt();
+		StdOut.println("Base -> Representation of " + num);
+		for (int base = 2; base <= 16; base++) {
+			StdOut.println(base + " -> " + convertNum(num, base));
+		}
 	}
 
 }
