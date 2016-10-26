c04fe7d5350b2ec22d51577424cfadf9094b027c
Soujirou152 <her.pengxue@gmail.com>
2016-10-26 03:48:09 -0500

    Finished #3 or almost did

--- 602_hw_4/src/RandomList.java
+++ 602_hw_4/src/RandomList.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
+import java.util.Random;
 
-public class RandomList
-{
-	public static void main(String[] args)
-	{
-	
+public class RandomList {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		ArrayList<Integer> randomList = new ArrayList<Integer>();
+		for (int i = 0; i < 100; i++) {
+			randomList.add(rand.nextInt(101));
+		}
+		int i = 0;
+
+		StdOut.println("Enter an index of an array. (0-99): ");
+		try {
+			i = StdIn.readInt();
+			StdOut.println(randomList.get(i));
+		} catch (IndexOutOfBoundsException oob) {
+			StdOut.println("Out of Bounds");
+			System.exit(1);
+
+		}
 	}
 }
