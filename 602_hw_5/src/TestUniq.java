55d44d9e85dfceafd429e45732e875b3a1c1ca29
Soujirou152 <her.pengxue@gmail.com>
2016-11-12 01:30:40 -0600

    Finished JUNIT testing

--- 602_hw_5/src/TestUniq.java
+++ 602_hw_5/src/TestUniq.java
@@ -27,6 +27,46 @@ public class TestUniq
 	@Test
 	public void testShortDupe()
 	{
+		ArrayList<String> al = buildTest(new String[]{"one","one"});
+		
+		assertEquals("two duplicate Strings","[one]", (Uniq.<String>removeDuplicates(al)).toString());
+	}
+	
+	@Test
+	public void test1()
+	{
+		ArrayList<String> al = buildTest(new String[]{"one","one","one", "two"});
+		
+		assertEquals("two duplicate Strings","[one, two]", (Uniq.<String>removeDuplicates(al)).toString());
+	}
+	
+	@Test
+	public void test2()
+	{
+		ArrayList<String> al = buildTest(new String[]{"elephant","Tiger","elephant","Elephant","lion"});
+		
+		assertEquals("two duplicate Strings","[elephant, Tiger, Elephant, lion]", (Uniq.<String>removeDuplicates(al)).toString());
+	}
+	
+	@Test
+	public void test3Integer()
+	{
+		ArrayList<Integer> al = buildTest(new Integer[]{1,2,3,1,2,3});
+		
+		assertEquals("two duplicate Strings","[1, 2, 3]", (Uniq.<Integer>removeDuplicates(al)).toString());
+	}
+	
+	@Test
+	public void test4Integer()
+	{
+		ArrayList<Integer> al = buildTest(new Integer[]{1242131,1242131, 111, 120});
+		
+		assertEquals("two duplicate Strings","[1242131, 111, 120]", (Uniq.<Integer>removeDuplicates(al).toString()));
+	}
+	
+	@Test
+	public void test5()
+	{
 		ArrayList<String> al = buildTest(new String[]{"one","one","one", "two"});
 		
 		assertEquals("two duplicate Strings","[one, two]", (Uniq.<String>removeDuplicates(al)).toString());
